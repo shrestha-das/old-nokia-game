2a02a85daaac922dba85f2bc8426c90b8e5df799
shrestha-das <shresthadas.0.infinity@gmail.com>
2025-11-13 22:48:56 +0530

    impure 2D array created

--- game.js
+++ game.js
@@ -1,12 +1,24 @@
 const board = document.querySelector(".board");
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 80;
+const blockWidth = 80;
+
+const blocks = [];
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight/ blockHeight);
 
-for(let i=0; i< cols*rows; i++) {
-    const block = document.createElement("div");
-    block.classList.add("blocks");
-    board.appendChild(block);
+// for(let i=0; i< cols*rows; i++) {
+//     const block = document.createElement("div");
+//     block.classList.add("blocks");
+//     board.appendChild(block);
+// }
+
+for(let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+        block.innerText = `${row}, ${col}`;
+        blocks[`${row},${col}`] = block; 
+    }
 }
